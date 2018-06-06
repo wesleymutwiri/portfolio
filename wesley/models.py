3a5a512257c391ff5ca025301ac1e09214150f97
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-06 15:03:38 +0300

    added new images model

--- wesley/models.py
+++ wesley/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import datetime as dt 
+
 
 # Create your models here.
+class Images(models.Model):
+    image_name = models.CharField(max_length=30, blank=True)
+    image_description = models.CharField(max_length=120,blank= True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'articles/', blank=True)
+
+    class Meta:
+        ordering = ['-pub_date']   
+    
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete() 
+    
+    @classmethod
+    def update_image(cls,id,target,update):
+        update = cls.objects.filter(id=id).update(target=update)
+        return update
+    
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.order_by('-pub_date')
+        return images
+
+    @classmethod
+    def get_image_by_Id(cls, image_id):
+      return cls.objects.get(pk=image_id)
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        img = cls.objects.filter(category__category__icontains=search_term)
+        return img
--- wesley/views.py
+++ wesley/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import datetime as dt 
+
 
 # Create your views here.
 def index(request):
+    date = dt.date.today
     return render(request,'wesley/index.html')
