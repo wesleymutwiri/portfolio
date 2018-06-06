50ad353739507dc4ea1998c044a3b0fea2076811
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-06 15:09:25 +0300

    additional components to the admin

--- wesley/admin.py
+++ wesley/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Images 
 
 # Register your models here.
+
+# class ImagesAdmin(admin.ModelAdmin):
+    # filter_horizontal = ('category')
+admin.site.register(Images)
