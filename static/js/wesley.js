c22f1b27bdc43367c87fed656a2529681d89ee80
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-07 22:48:42 +0300

    added js, css and html

---
+++ static/js/wesley.js
@@ -0,0 +1,27 @@
+// particlesJS("particles", {
+//   "particles": {
+//     "number": {
+//       "value": 80,
+//       "density": {
+//         "enable": true,
+//         "value_area": 800
+//       }
+//     },
+//     "color": {
+//       "value": "#ffffff"
+//     },
+//     "shape": {
+//       "type": "circle",
+//       "stroke": {
+//         "width": 0,
+//         "color": "#000000"
+//       },
+//       "polygon": {
+//         "nb_sides": 5
+//       },
+//       "image": {
+//         "src": "img/github.svg",
+//         "width": 100,
+//         "height": 100
+//       }
+//     },
