498b6c88235dfd7cd7a4c12042a4339d00bcef6c
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-11 12:33:35 +0300

    change to js

--- static/js/wesley.js
+++ static/js/wesley.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - 69
-            }, 4    00, function () {
+            }, 600, function () {
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
